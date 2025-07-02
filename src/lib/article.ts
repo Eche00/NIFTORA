55fc44f92819f5ebbfbf6864f08d1050cfe79dca
Eche00 <echeeze956@gmail.com>
2025-07-02 22:33:39 +0100

    Article lib for card details

---
+++ src/lib/article.ts
@@ -0,0 +1,5 @@
+export const articles=[
+  {img:'/Hero.png',title:'We’ve lowered market prices on all your favourite NFTs',date:'Jan 11, 2025'},
+  {img:'/Hero.png',title:'Your Ultimate Guide to Getting Started With NFTs on Rarible',date:'Feb 23, 2025'},
+  {img:'/Hero.png',title:'Why Community Marketplaces are the future of NFT trading',date:'March 6, 2025'},
+]
