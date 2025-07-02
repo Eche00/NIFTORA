e9a0680d075fc3071a592cfcfdacf023eb9ce45e
Eche00 <echeeze956@gmail.com>
2025-07-02 22:33:49 +0100

    Top collection lib for card details

---
+++ src/lib/topcollection.ts
@@ -0,0 +1,12 @@
+
+
+export const topcollections=[
+  {img:'/Hero.png',title:'Proud Ape #00',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:4.99},
+  {img:'/Hero.png',title:'Proud Ape #01',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #02',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #03',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #04',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #05',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #06',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/Hero.png',title:'Proud Ape #07',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+]
