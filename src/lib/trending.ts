93ae2138cd6346f9c916ba5b1ec9e334eb041d24
Eche00 <echeeze956@gmail.com>
2025-07-02 22:34:02 +0100

    Trending lib for card details

---
+++ src/lib/trending.ts
@@ -0,0 +1,25 @@
+
+const ethLogo = (`<svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect y="0.5" width="48" height="48" rx="24" fill="white" fill-opacity="0.33"/>
+<circle cx="24" cy="24.5" r="20.1905" fill="#5B40FF"/>
+<g clip-path="url(#clip0_170_422)">
+<path d="M24.3038 31.5047L15.0791 26.0608L24.2982 39.0569L33.5276 26.0608L24.2982 31.5047H24.3038ZM24.4409 9.005L15.22 24.3094L24.4409 29.7627L33.6656 24.315L24.4409 9.005Z" fill="white"/>
+</g>
+<defs>
+<clipPath id="clip0_170_422">
+<rect width="30.0509" height="30.0509" fill="white" transform="translate(9.44434 9.94409)"/>
+</clipPath>
+</defs>
+</svg>`)
+
+export const trending=[
+  {img:'/Hero.png',title:'Proud Ape #00',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:4.99,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #01',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #02',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #03',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #04',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #05',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #06',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #07',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+  {img:'/Hero.png',title:'Proud Ape #08',subtitle:'Ape',latestbid:'7.6 ETH',from:'2.9 ETH',priceinusd:'3,049',x:8.92,logo :ethLogo},
+]
