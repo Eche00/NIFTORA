f2ffd6b03f382713884e85adb9719c185374e1d8
Eche00 <echeeze956@gmail.com>
2025-07-03 01:21:56 +0100

    article fix

--- src/lib/article.ts
+++ src/lib/article.ts
@@ -1,5 +1,5 @@
 export const articles=[
-  {img:'/Hero.png',title:'We’ve lowered market prices on all your favourite NFTs',date:'Jan 11, 2025'},
-  {img:'/Hero.png',title:'Your Ultimate Guide to Getting Started With NFTs on Rarible',date:'Feb 23, 2025'},
-  {img:'/Hero.png',title:'Why Community Marketplaces are the future of NFT trading',date:'March 6, 2025'},
+  {img:'/article1.jpg',title:'We’ve lowered market prices on all your favourite NFTs',date:'Jan 11, 2025'},
+  {img:'/article2.jpg',title:'Your Ultimate Guide to Getting Started With NFTs on Rarible',date:'Feb 23, 2025'},
+  {img:'/article3.jpg',title:'Why Community Marketplaces are the future of NFT trading',date:'March 6, 2025'},
 ]
