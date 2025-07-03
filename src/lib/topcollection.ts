53da71ba00f668b5b17d31448305ff7fc85627db
Eche00 <echeeze956@gmail.com>
2025-07-03 01:35:33 +0100

    Update top collections

--- src/lib/topcollection.ts
+++ src/lib/topcollection.ts
@@ -1,12 +1,12 @@
 
 
 export const topcollections=[
-  {img:'/Hero.png',title:'Proud Ape #00',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:4.99},
-  {img:'/Hero.png',title:'Proud Ape #01',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #02',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #03',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #04',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #05',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #06',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
-  {img:'/Hero.png',title:'Proud Ape #07',subtitle:'Ape',floorprice:'7.6 ETH',priceinusd:'3,049',x:8.92},
+  {img:'/collection1.jpg',title:'COOLSKULL',subtitle:'Coolskull.io',floorprice:'0.9 ETH',priceinusd:'1480',x:14.34},
+  {img:'/collection2.jpg',title:'PUNKKID',subtitle:'Punkkids.co',floorprice:'1.4 ETH',priceinusd:'2,478',x:23.21},
+  {img:'/collection3.jpg',title:'DAN$',subtitle:'dan.co',floorprice:'0.3 ETH',priceinusd:'567',x:4.34},
+  {img:'/collection4.jpg',title:'GOOGLE GIRL',subtitle:'Coolskull.io',floorprice:'1.2 ETH',priceinusd:'1700',x:4.34},
+  {img:'/collection5.jpg',title:'GANGKID',subtitle:'Coolskull.io',floorprice:'0.9 ETH',priceinusd:'1480',x:4.34},
+  {img:'/collection6.jpg',title:'COOLSKULL',subtitle:'Coolskull.io',floorprice:'0.9 ETH',priceinusd:'1480',x:4.34},
+  {img:'/collection7.jpg',title:'STARK',subtitle:'Stark.io',floorprice:'0.9 ETH',priceinusd:'1480',x:4.34},
+  {img:'/collection8.jpg',title:'BASEB',subtitle:'Baseb.co',floorprice:'0.9 ETH',priceinusd:'1480',x:4.34},
 ]
