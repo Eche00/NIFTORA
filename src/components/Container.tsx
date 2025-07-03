768923f769591bba3d728a2a0c40cce6de33975a
Eche00 <echeeze956@gmail.com>
2025-07-03 14:45:42 +0100

    Add MobileScreen component for responsive design in Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,17 +1,22 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import { Outlet } from "react-router";
+import MobileScreen from "./MobileScreen";
 
 function Container() {
   return (
     <div className=" bg-[#121212]">
-      <Header/>
+    <section className=" bg-[#121212] block md:hidden"><MobileScreen/></section>
+    
+    <main className=" bg-[#121212] md:block hidden">
+        <Header/>
      <section className="w-full pt-[80px] z-10 ">
        <Outlet/>
      </section>
       <section className=" z-30">
         <Footer/>
       </section>
+    </main>
     </div>
   )
 }
