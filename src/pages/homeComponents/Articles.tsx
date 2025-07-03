3b825ec5cc7a606d7dc33fe921268204f19cd343
Eche00 <echeeze956@gmail.com>
2025-07-03 11:21:53 +0100

    Enhance Articles component with framer-motion for improved animations

--- src/pages/homeComponents/Articles.tsx
+++ src/pages/homeComponents/Articles.tsx
@@ -1,15 +1,16 @@
+import { motion } from "framer-motion";
 import { articles } from "../../lib/article";
 
 function Articles() {
   return  <div className=" relative ">
              
              <main className="px-[104px] pt-[64px] pb-[164px] flex flex-col relative">
-                <h1 className="text-[48px] font-[700] leading-[120px] text-center text-transparent bg-clip-text bg-gradient-to-r  from-[#A849FF] to-[#F9FBFF] ">
-                  Articles
-                </h1>
+               <motion.h1 initial={{opacity:0,y:100}} whileInView={{opacity:1, y:1}} transition={{duration:2}} viewport={{ once: true }} className="text-[48px] font-[700] leading-[120px] text-center text-transparent bg-clip-text bg-gradient-to-r  from-[#A849FF] to-[#F9FBFF]">
+           Articles
+         </motion.h1>
            
           {/* cards  */}
-            <section className=" flex flex-wrap items-center gap-[24px] gap-y-[40px] justify-center relative py-[100px] max-w-[95%] mx-auto">
+            <motion.section initial={{opacity:0,scale:0.5}} whileInView={{opacity:1, scale:1}} transition={{duration:2}} viewport={{ once: true }} className=" flex flex-wrap items-center gap-[24px] gap-y-[40px] justify-center relative py-[100px] max-w-[95%] mx-auto">
                   
                      {/* each card  */}
                      {articles.map((card)=>(
@@ -22,10 +23,19 @@ function Articles() {
                         </div>
                       </div>
                       ))} 
-                    </section>
+                    </motion.section>
              {/* see more button  */}
        <div className=' w-full flex items-center justify-center'>
-          <button className=' text-[24px] text-[#E0E0E0] font-[600] bg-transparent border-[1px] border-[#E0E0E0] px-[32px] py-[10px] rounded-[8px] w-fit'>See more</button>
+          <motion.button                   whileInView={{
+                    rotate: [0, -10, 10, -10, 10, 0],
+                  }}
+                  transition={{
+                    duration: 5,
+                    delay: 1,
+                    repeatDelay: 10,
+                    repeat: Infinity,
+                    repeatType: "loop", // (optional) smoother looping
+                  }} className=' text-[24px] text-[#E0E0E0] font-[600] bg-transparent border-[1px] border-[#E0E0E0] px-[32px] py-[10px] rounded-[8px] w-fit'>See more</motion.button>
        </div>
              </main>
            </div>;;
