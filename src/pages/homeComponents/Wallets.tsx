aaf771b55b780c40cc4b412f480bfdf1013cb349
Eche00 <echeeze956@gmail.com>
2025-07-03 11:23:00 +0100

    Enhance Wallets component with framer-motion for improved animations and transitions

--- src/pages/homeComponents/Wallets.tsx
+++ src/pages/homeComponents/Wallets.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 
 
 function Wallets() {
@@ -6,13 +7,13 @@ function Wallets() {
       {/* container  */}
       <main className="  px-[104px] text-white flex flex-col items-center gap-[64px]">
       <section className="flex items-center gap-[64px]">
-        <img src="/metamask.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-        <img src="/trustwallet.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-        <img src="/walletconnect.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+        <motion.img initial={{opacity:0,x:-200}} whileInView={{opacity:1, x:0}} transition={{duration:2}} viewport={{ once: true }} src="/metamask.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+        <motion.img initial={{opacity:0,y:200}} whileInView={{opacity:1, y:0,}} transition={{duration:2}} viewport={{ once: true }} src="/trustwallet.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+        <motion.img initial={{opacity:0,x:200}} whileInView={{opacity:1,x:0}} transition={{duration:2}} viewport={{ once: true }} src="/walletconnect.png" alt="" className=" w-[285px] h-[80px] object-contain" />
       </section>
       <section className="flex items-center gap-[64px]">
-                <img src="/exodus.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-                <img src="/safepal.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+                <motion.img initial={{opacity:0,y:200,x:-200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/exodus.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+                <motion.img initial={{opacity:0,y:200,x:200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/safepal.png" alt="" className=" w-[285px] h-[80px] object-contain" />
 
       </section>
       </main>
