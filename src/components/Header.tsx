7b9bf7da9c366f9a39007e05b822c2f816ba36fc
Eche00 <echeeze956@gmail.com>
2025-07-04 04:24:04 +0100

    Enhance Header component for improved layout and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,11 @@ function Header() {
     <div className="bg-[#121212] fixed top-0 left-0 w-full z-50">
      
       {/* container  */}
-     <main className="sm:px-[104px] px-[20px] py-[20px] flex items-center justify-between bg-[#121212] z-50 relative">
+     <main className="md:px-[104px] px-[20px] py-[20px] flex items-center justify-between bg-[#121212] z-50 relative">
        {/* logo */}
       <section className=" ">{HeaderIcon.logo}</section>
       {/* navigation  */}
-      <section className="hidden sm:flex items-center gap-[24px] text-white text-[16px] font-[500]">
+      <section className="hidden md:flex items-center gap-[24px] text-white text-[16px] font-[500]">
         <p className=" cursor-pointer text-[#B76AFF]">Home</p>
         <p className=" cursor-pointer">Services</p>
         <p className=" cursor-pointer">Explore</p>
@@ -24,8 +24,8 @@ function Header() {
       </section>
 
       {/* connect wallet  */}
-      <button className=" px-[32px] py-[10px] text-[16px] font-[] rounded-[8px] bg-gradient-to-b from-[#1F51FF] to-[#8A2BE2] text-white cursor-pointer hover:scale-[105%] transition-all duration-300 hidden sm:inline-block">Connect Wallet</button>
-      <button className="  text-white text-2xl cursor-pointer hover:scale-[105%] sm:hidden inline-block" onClick={()=>setOpenMenu(!openMenu)}>{openMenu ? <MdClose/>:<FaBars/>}</button>
+      <button className=" px-[32px] py-[10px] text-[16px] font-[] rounded-[8px] bg-gradient-to-b from-[#1F51FF] to-[#8A2BE2] text-white cursor-pointer hover:scale-[105%] transition-all duration-300 hidden md:inline-block">Connect Wallet</button>
+      <button className="  text-white text-2xl cursor-pointer hover:scale-[105%] md:hidden inline-block" onClick={()=>setOpenMenu(!openMenu)}>{openMenu ? <MdClose/>:<FaBars/>}</button>
      
      </main>
       <AnimatePresence>
@@ -46,7 +46,7 @@ function Header() {
            <button  className=" px-[32px] py-[10px] text-[16px] mx-[10px] rounded-[8px] bg-gradient-to-b from-[#1F51FF] to-[#8A2BE2] text-white cursor-pointer hover:scale-[105%] transition-all duration-300 ">Connect Wallet</button>
 
             {/* bg */}
-         <span className=" circleBlurLH sm:hidden inline-block"></span>
+         <span className=" circleBlurLH md:hidden inline-block"></span>
         </motion.section>
         }
         </AnimatePresence>
