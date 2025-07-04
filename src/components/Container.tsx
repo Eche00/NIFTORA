a8751bc209dc8d6a998379aa622df7ea9fe80b54
Eche00 <echeeze956@gmail.com>
2025-07-04 02:00:05 +0100

    Remove unused MobileScreen component from Container for cleaner code

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,12 @@
 import Footer from "./Footer"
 import Header from "./Header"
 import { Outlet } from "react-router";
-import MobileScreen from "./MobileScreen";
 
 function Container() {
   return (
     <div className=" bg-[#121212]">
-    <section className=" bg-[#121212] block md:hidden"><MobileScreen/></section>
+    {/* <section className=" bg-[#121212] block md:hidden"><MobileScreen/></section> */}
     
-    <main className=" bg-[#121212] md:block hidden">
         <Header/>
      <section className="w-full pt-[80px] z-10 ">
        <Outlet/>
@@ -16,7 +14,6 @@ function Container() {
       <section className=" z-30">
         <Footer/>
       </section>
-    </main>
     </div>
   )
 }
