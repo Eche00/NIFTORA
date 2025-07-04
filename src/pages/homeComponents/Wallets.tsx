978f083e3975ff852f898c2ab3b704eb4491df66
Eche00 <echeeze956@gmail.com>
2025-07-04 02:49:31 +0100

    Refactor Wallets component for improved layout and responsiveness

--- src/pages/homeComponents/Wallets.tsx
+++ src/pages/homeComponents/Wallets.tsx
@@ -5,15 +5,15 @@ function Wallets() {
   return (
     <div>
       {/* container  */}
-      <main className="  px-[104px] text-white flex flex-col items-center gap-[64px]">
-      <section className="flex items-center gap-[64px]">
-        <motion.img initial={{opacity:0,x:-200}} whileInView={{opacity:1, x:0}} transition={{duration:2}} viewport={{ once: true }} src="/metamask.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-        <motion.img initial={{opacity:0,y:200}} whileInView={{opacity:1, y:0,}} transition={{duration:2}} viewport={{ once: true }} src="/trustwallet.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-        <motion.img initial={{opacity:0,x:200}} whileInView={{opacity:1,x:0}} transition={{duration:2}} viewport={{ once: true }} src="/walletconnect.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+      <main className="  sm:px-[104px] px-[10px] text-white flex items-center justify-center flex-col w-full  sm:gap-[64px] gap-[10px]">
+      <section className="flex items-center justify-center flex-wrap sm:gap-[64px] gap-[10px]">
+        <motion.img initial={{opacity:0,x:-200}} whileInView={{opacity:1, x:0}} transition={{duration:2}} viewport={{ once: true }} src="/metamask.png" alt="" className=" sm:w-[285px] sm:h-[80px] w-[220px] h-[60px] object-contain" />
+        <motion.img initial={{opacity:0,y:200}} whileInView={{opacity:1, y:0,}} transition={{duration:2}} viewport={{ once: true }} src="/trustwallet.png" alt="" className=" sm:w-[285px] sm:h-[80px] w-[220px] h-[60px] object-contain" />
+        <motion.img initial={{opacity:0,x:200}} whileInView={{opacity:1,x:0}} transition={{duration:2}} viewport={{ once: true }} src="/walletconnect.png" alt="" className=" sm:w-[285px] sm:h-[80px] w-[220px] h-[60px] object-contain" />
       </section>
-      <section className="flex items-center gap-[64px]">
-                <motion.img initial={{opacity:0,y:200,x:-200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/exodus.png" alt="" className=" w-[285px] h-[80px] object-contain" />
-                <motion.img initial={{opacity:0,y:200,x:200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/safepal.png" alt="" className=" w-[285px] h-[80px] object-contain" />
+      <section className="flex items-center justify-center flex-wrap sm:gap-[64px] gap-[10px]">
+                <motion.img initial={{opacity:0,y:200,x:-200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/exodus.png" alt="" className=" sm:w-[285px] sm:h-[80px] w-[220px] h-[60px] object-contain" />
+                <motion.img initial={{opacity:0,y:200,x:200}} whileInView={{opacity:1, y:0,x:0}} transition={{duration:2}} viewport={{ once: true }}src="/safepal.png" alt="" className=" sm:w-[285px] sm:h-[80px] w-[220px] h-[60px] object-contain" />
 
       </section>
       </main>
